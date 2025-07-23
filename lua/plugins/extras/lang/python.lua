cd09455e493fda6713f2b48270217c6e6d0f08dc
BlueDruddigon <haibuihoang1999@gmail.com>
2025-07-23 13:09:25 +0700

    fix(formatter): rename the correct formatter for python files

--- lua/plugins/extras/lang/python.lua
+++ lua/plugins/extras/lang/python.lua
@@ -3,6 +3,6 @@ return {
   {
     "stevearc/conform.nvim",
     dependencies = { "mason.nvim" },
-    opts = { formatters_by_ft = { python = { "ruff_organize_imports", "ruff_lsp" } } },
+    opts = { formatters_by_ft = { python = { "ruff_organize_imports", "ruff_format" } } },
   },
 }
