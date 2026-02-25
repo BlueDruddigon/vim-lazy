2c79c0367b9e393ba0cc6ccfefa71f3f816baba8
BlueDruddigon <haibh.kma@gmail.com>
2026-02-25 17:25:14 +0700

    feat(claude-code): configure plugin

---
+++ lua/plugins/extras/ai/claudecode.lua
@@ -0,0 +1,9 @@
+return {
+  "coder/claudecode.nvim",
+  dependencies = { "folke/snacks.nvim" },
+  opts = {
+    terminal = {
+      snacks_win_opts = { position = "float", width = 0.9, height = 0.9 },
+    },
+  },
+}
